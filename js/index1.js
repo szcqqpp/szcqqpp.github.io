f7fb8dd1f025c38dc4fa6f0f09886a889bb051ea
szcqqpp <48516732+szcqqpp@users.noreply.github.com>
2019-04-23 19:40:51 +0800

    更新

--- js/index1.js
+++ js/index1.js
@@ -1,5 +1,15 @@
 $(function(){
-	$(".off").click(function(){
-		$(".box .page1").css({"transform":"rotatex(-45deg)"});
+	$(".asd").click(function(){
+		$(".box .page1").css({"transform":"rotatex(-30deg)",transitionDelay:"0s"});
+		$(".nav").css({bottom:0,transitionDelay:"2s"}).addClass("active");
+		$(".nav li").css({transitionDelay:"3s"});
+		$("#of").css({transform:"rotate(0deg)",transitionDelay:"3s"});
+	})
+	$("#of").click(function(){
+		console.log(1)
+		$(".box .page1").css({"transform":"rotatex(0deg)",transitionDelay:"1.5s"});
+		$(".nav").css({bottom:"-200px",transitionDelay:"1s"}).removeClass("active");
+		$(".nav li").css({transitionDelay:"0s"});
+		$("#of").css({transform:"rotate(45deg)",transitionDelay:"0s"});
 	})
 })
